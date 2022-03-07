c113b6e159d2aad622e04a87c923a036a1bf96b6
AnastasiyaSmirnova <ansmir10.99@gmail.com>
2022-03-07 18:58:29 +0300

    create basic components

--- database-frontend/src/app/app.module.ts
+++ database-frontend/src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { RouterModule, Routes } from '@angular/router'
+import { HeaderComponent } from './header/header.component'
+import { AnimalsComponent } from './animals/animals.component'
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component'
+import { HomeComponent } from './home/home.component'
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'animals', component: AnimalsComponent},
+  {path: '**', component: NotFoundPageComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AnimalsComponent,
+    NotFoundPageComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
